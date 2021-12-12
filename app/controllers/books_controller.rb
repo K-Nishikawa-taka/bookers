f100409f1c9455752a027f678646db00c24f466d
EC2 Default User <ec2-user@ip-172-31-0-111.ec2.internal>
2021-12-12 02:32:36 +0000

    initial

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,15 @@
 class BooksController < ApplicationController
   def index
-    @books = Book.all
     @book = Book.new
+    @books = Book.all
   end
 
   def create
-    book = Book.new(book_params)
-    if book.save
+    @book = Book.new(book_params)
+    @books = Book.all
+    if @book.save
       flash[:notice] = "Book was saccessfully created."
-      redirect_to book_path(book.id)
+      redirect_to book_path(@book.id)
     else
       render :index
     end
@@ -23,10 +24,10 @@ class BooksController < ApplicationController
   end
   
   def update
-    book = Book.find(params[:id])
-    if book.update(book_params)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
       flash[:notice] = "Book was saccessfully updated."
-      redirect_to book_path(book)
+      redirect_to book_path(@book.id)
     else
       render :edit
     end
